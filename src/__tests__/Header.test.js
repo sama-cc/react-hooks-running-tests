743f7007dbf8fcd1441e99fea85c7107ade89bb4
Sam <sam.camhi@gmail.com>
2023-05-21 19:36:02 -0400

    completed assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -7,5 +7,9 @@ import Header from "../components/Header";
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
